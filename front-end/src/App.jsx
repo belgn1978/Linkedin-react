ecee6d3498a4201e382d2def72508a7816a2f792
Matt <68106254+belgn1978@users.noreply.github.com>
2025-11-27 19:55:50 +0000

    First commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,44 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticleListPage from "./pages/ArticlesList";
+import ArticlePage from "./pages/ArticlePage";
+import NavBar from "./NavBar";
+import Layout from "./Layout";
+
+const routes = [
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/about",
+        element: <AboutPage />,
+      },
+      {
+        path: "/articles",
+        element: <ArticleListPage />,
+      },
+      {
+        path: "articles/individual",
+        element: <ArticlePage />,
+      },
+    ],
+  },
+];
+
+const router = createBrowserRouter(routes);
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
